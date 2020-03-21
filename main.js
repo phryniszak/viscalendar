0583ddd36dfd167216de2b12653b946392dcb2aa
Pawel Hryniszak <phryniszak@gmail.com>
2020-03-21 08:25:55 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,389 @@
+// https://developers.google.com/s/results/web/fundamentals?q=full%20screen
+console.log("*");
+var doc = window.document,
+    _scroller = {},
+    _settings = {},
+    _btnSettings = doc.getElementById("btnSettings"),
+    _templates = doc.getElementsByTagName("template").item(0).content,
+    _monthTemplate = _templates.querySelector("div.ph-month"),
+    _dayPrevTemplate = _templates.querySelector("li.month-prev"),
+    _dayCurrTemplate = _templates.querySelector("li.month-curr"),
+    _scrollerDiv = doc.querySelector("div.ph-scroller-wrapper"),
+    _shift;
+
+const FIRSTDAYOFWEEK = 1;
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+const _DATE_ZERO = new Date(2013, 0, 21);
+
+//
+//
+// utils:
+const _dateAdd = (date, delta) => {
+    date.setDate(date.getDate() + delta);
+};
+
+//
+//
+// utils:
+const _debounce = (func, delay) => {
+    let debounceTimer;
+    return function () {
+        const context = this;
+        const args = arguments;
+        clearTimeout(debounceTimer);
+        debounceTimer
+            = setTimeout(() => func.apply(context, args), delay);
+    };
+};
+
+//
+//
+// a and b are javascript Date objects
+const _dateDiffInDays = (a, b) => {
+    // Discard the time and time-zone information.
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+};
+
+//
+//
+//
+function MonthScroller(date) {
+
+    // const
+    const RUN_OFF = 12;
+
+    this.updateShift = () => {
+        _scrollerDiv.childNodes.forEach(monthEl => {
+            let date = new Date();
+            let month = date.setTime(monthEl.dataset.date);
+            console.log(month);
+        });
+    };
+
+    //
+    //
+    //
+    this.getClassFromDay = (date) => {
+
+        let timeDiff = (date - _DATE_ZERO);
+        if (timeDiff < 0) {
+            return _UNKNOWN_SHIFT;
+        }
+
+        let daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+        // more correct way:
+        // let daysDiff = _dateDiffInDays(day, _DATE_ZERO);
+
+        // is it days or nights
+        let ndShiftsModulo = daysDiff % (7 * 8);
+
+        switch (_shift) {
+            case "shift-A":
+                return shift.A[ndShiftsModulo] || _UNKNOWN_SHIFT;
+                break;
+            case "shift-B":
+                return shift.B[ndShiftsModulo] || _UNKNOWN_SHIFT;
+                break;
+            case "shift-C":
+                return shift.C[ndShiftsModulo] || _UNKNOWN_SHIFT;
+                break;
+            case "shift-D":
+                return shift.D[ndShiftsModulo] || _UNKNOWN_SHIFT;
+                break;
+        }
+
+        return _UNKNOWN_SHIFT;
+    };
+
+    //
+    //
+    //
+    this.getMonthFragment = (month) => {
+
+        const monthEl = _monthTemplate.cloneNode(true);
+        monthEl.dataset.date = month.getTime();
+
+        let _getDays = (month) => {
+            if (month === undefined || FIRSTDAYOFWEEK === undefined) {
+                return;
+            }
+            // First day of the month (at midnight).
+            var date = new Date(0, 0);
+            date.setFullYear(month.getFullYear());
+            date.setMonth(month.getMonth());
+            date.setDate(1);
+
+            // Rewind to first day of the week.
+            while (date.getDay() !== FIRSTDAYOFWEEK) {
+                _dateAdd(date, -1);
+            }
+
+            var days = [];
+            var startMonth = date.getMonth();
+            var targetMonth = month.getMonth();
+            while (date.getMonth() === targetMonth || date.getMonth() === startMonth) {
+                days.push(date.getMonth() === targetMonth ? new Date(date.getTime()) : null);
+
+                // Advance to next day.
+                _dateAdd(date, 1);
+            }
+            return days;
+        };
+
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
+        const yearAndMonth = month.toLocaleString("default", { month: "long", year: "numeric" });
+        monthEl.getElementsByTagName("header")[0].innerText = yearAndMonth;
+
+        const days = _getDays(month);
+        const daysTemplate = monthEl.querySelector("ul.day-grid");
+
+        let dayTemplate;
+        days.forEach((day) => {
+            if (day) {
+                dayTemplate = _dayCurrTemplate.cloneNode(true);
+                dayTemplate.innerText = day.getDate();
+                dayTemplate.classList.add(this.getClassFromDay(day));
+            } else {
+                dayTemplate = _dayPrevTemplate.cloneNode(true);
+            }
+            daysTemplate.appendChild(dayTemplate);
+        });
+
+        return monthEl;
+    };
+
+    //
+    //
+    //
+    this.initMonthTemplate = () => {
+
+        var date = new Date(0, 0);
+        // Rewind to first day of the week.
+        while (date.getDay() !== FIRSTDAYOFWEEK) {
+            _dateAdd(date, -1);
+        }
+
+        let dayNamesNodes = _templates.querySelectorAll("ul.weekdays >li");
+
+        let name = "";
+        dayNamesNodes.forEach((dayNode) => {
+            let dayName = dayNode.children;
+            name = date.toLocaleString("default", { weekday: "long" });
+            dayName[2].innerText = name;
+            name = date.toLocaleString("default", { weekday: "short" });
+            dayName[1].innerText = name;
+            dayName[0].innerText = name[0];
+            _dateAdd(date, 1);
+        });
+    };
+
+    //
+    //
+    //
+    this.scrollUpdate = () => {
+
+        let topRect = _scrollerDiv.firstElementChild.getBoundingClientRect();
+        let bottomRect = _scrollerDiv.lastElementChild.getBoundingClientRect();
+        let contextRect = _scrollerDiv.parentElement.getBoundingClientRect();
+        let scrollPosTop = contextRect.top - topRect.top;
+        let scrollPosBottom = bottomRect.bottom - contextRect.bottom;
+        let elHight = (topRect.height + bottomRect.height) / 2;
+
+        // add element
+        if (scrollPosTop / elHight < RUN_OFF) {
+
+            let date = new Date();
+            date.setTime(_scrollerDiv.firstElementChild.dataset.date);
+
+            let el = this.getMonthFragment(new Date(date.setMonth(date.getMonth() - 1)));
+            _scrollerDiv.insertBefore(el, _scrollerDiv.firstElementChild);
+
+        } else if (scrollPosBottom / elHight < RUN_OFF) {
+
+            let date = new Date();
+            date.setTime(_scrollerDiv.lastElementChild.dataset.date);
+
+            let el = this.getMonthFragment(new Date(date.setMonth(date.getMonth() + 1)));
+            _scrollerDiv.appendChild(el);
+        }
+
+        // this.scrollUpdateDebounce();
+    };
+
+
+    //
+    //
+    // TODO: remove excessive elements
+    this.scrollUpdateDebounce = _debounce(() => console.log("debounced"), 100);
+
+    //
+    //
+    //
+    this.initLayout = (date) => {
+
+        // First day of the month (at midnight).
+        // some strange things can happen if we stuck with 31 day of the month
+        var date1st = new Date(0, 0);
+        date1st.setFullYear(date.getFullYear());
+        date1st.setMonth(date.getMonth());
+        date1st.setDate(1);
+
+        // starting element
+        let el1st = this.getMonthFragment(date1st);
+        _scrollerDiv.appendChild(el1st);
+
+        // now add elements in both side
+        let topRect, bottomRect, scrollInsideHeight;
+        let contextRect = _scrollerDiv.parentElement.getBoundingClientRect();
+        let monthShift = 0;
+        do {
+            monthShift++;
+            let el = this.getMonthFragment(new Date(date1st.getFullYear(), date1st.getMonth() + monthShift), 1);
+            _scrollerDiv.appendChild(el);
+            el = this.getMonthFragment(new Date(date1st.getFullYear(), date1st.getMonth() - monthShift), 1);
+            _scrollerDiv.insertBefore(el, _scrollerDiv.firstElementChild);
+
+            topRect = _scrollerDiv.firstElementChild.getBoundingClientRect();
+            bottomRect = _scrollerDiv.lastElementChild.getBoundingClientRect();
+            scrollInsideHeight = bottomRect.bottom - topRect.top;
+        }
+        while ((scrollInsideHeight / contextRect.height) < (2 * RUN_OFF + 1));
+
+        // move to date to center
+        el1st.scrollIntoView();
+
+        // save offset
+        this._offset = -el1st.offsetTop;
+    };
+
+    // On resize
+    this.resize = () => {
+        console.log("Scroller resize");
+    };
+
+    // init month template with week days
+    this.initMonthTemplate();
+
+    // init layout
+    this.initLayout(date);
+
+    // scroll events
+    _scrollerDiv.parentNode.onscroll = this.scrollUpdate;
+}
+
+//
+//
+//
+function SettingsModal() {
+
+    // Gloabl state variables
+    const STATE_CLOSED = 1;
+    const STATE_OPEN = 2;
+    let currentState = STATE_CLOSED;
+
+    this.click = (ev) => {
+        console.log("settings click:", ev);
+
+        switch (currentState) {
+            case STATE_CLOSED:
+                currentState = STATE_OPEN;
+                break;
+            case STATE_OPEN:
+                currentState = STATE_CLOSED;
+                break;
+
+            default:
+                currentState = STATE_CLOSED;
+                break;
+        }
+
+        this.updateState();
+    };
+
+    this.updateState = () => {
+        switch (currentState) {
+            case STATE_OPEN:
+                obfuscator.classList.add("is-visible");
+                modal.style.display = "flex";
+                break;
+            default:
+                obfuscator.classList.remove("is-visible");
+                modal.style.display = "none";
+                break;
+        }
+    };
+
+    this.resize = () => {
+        currentState = STATE_CLOSED;
+        this.updateState();
+    };
+
+    function inputClick(ev) {
+        // cancel other inputs
+        inputs.forEach(element => {
+            if (element !== ev.target) {
+                element.parentElement.MaterialCheckbox.uncheck();
+            }
+        });
+        // save selection
+        localStorage.setItem("shift", ev.target.id);
+    }
+
+    function inputInit() {
+        // save selection
+        let shiftId = localStorage.getItem("shift");
+        inputs.forEach(element => {
+            if (element.id === shiftId) {
+                element.parentElement.MaterialCheckbox.check();
+            }
+        });
+    }
+
+    // obfuscator
+    let obfuscator = doc.querySelector("div.mdl-layout__obfuscator");
+    obfuscator.addEventListener("click", this.click);
+    // modal
+    let modal = doc.getElementById("modalSettings");
+    // inputs
+    let inputs = modal.querySelectorAll(".settings-list-control input");
+    inputs.forEach(element => element.onclick = inputClick);
+    inputInit();
+}
+
+//
+//
+//
+function init() {
+
+    // init shift
+    let shift = localStorage.getItem("shift");
+    if (!shift) {
+        localStorage.setItem("shift", "shift-C");
+    }
+    _shift = localStorage.getItem("shift");
+
+    console.log(_shift);
+
+    // init callendar
+    _scroller = new MonthScroller(new Date());
+    _scroller.resize();
+
+    // init settings modal
+    _settings = new SettingsModal();
+    _btnSettings.addEventListener("click", _settings.click);
+}
+
+//
+//
+//
+function resize() {
+    _settings.resize();
+    _scroller.resize();
+}
+
+window.onload = init;
+window.onresize = resize;
---
+++ shifts.js
@@ -0,0 +1,190 @@
+const _DAY_SHIFT = "day-shift";
+const _NIGHT_SHIFT = "night-shift";
+const _START_NIGHT_SHIFT = "st-night-shift";
+const _END_NIGHT_SHIFT = "en-night-shift";
+const _DAY_OFF = "day-off";
+const _UNKNOWN_SHIFT = "day-unknown";
+
+var shift = {};
+
+shift.A = [7*8];
+shift.B = [7*8];
+shift.C = [7*8];
+shift.D = [7*8];
+
+// -------------------------------------
+// 35 1-1. MON
+shift.C[0]= _END_NIGHT_SHIFT;
+shift.D[0]= _DAY_SHIFT;
+// 1-2. TUE
+shift.C[1]=_DAY_OFF;
+shift.D[1]=_DAY_OFF;
+// 1-3. WED
+shift.C[2]=_DAY_OFF;
+shift.D[2]=_START_NIGHT_SHIFT;
+// 1-4. THU
+shift.C[3]=_DAY_SHIFT;
+shift.D[3]=_NIGHT_SHIFT;
+// 1-5. FRI
+shift.C[4]=_DAY_SHIFT;
+shift.D[4]=_END_NIGHT_SHIFT;
+// 1-6. SAT
+shift.C[5]=_DAY_OFF;
+shift.D[5]=_DAY_OFF;
+// 1-7. SUN
+shift.C[6]=_DAY_OFF;
+shift.D[5]=_DAY_OFF;
+// -------------------------------------
+// 36 2-1. MON
+shift.C[7]=_DAY_OFF;
+shift.D[7]=_START_NIGHT_SHIFT;
+// 2-2. TUE
+shift.C[8]=_DAY_SHIFT;
+shift.D[8]=_NIGHT_SHIFT;
+// 2-3. WED
+shift.C[9]=_DAY_SHIFT;
+shift.D[9]=_END_NIGHT_SHIFT;
+// 2-4. THU
+shift.C[10]=_DAY_OFF;
+shift.D[10]=_DAY_OFF;
+// 2-5. FRI
+shift.C[11]=_DAY_OFF;
+shift.D[11]=_START_NIGHT_SHIFT;
+// 2-6. SAT
+shift.C[12]=_DAY_SHIFT;
+shift.D[12]=_NIGHT_SHIFT;
+// 2-7. SUN
+shift.C[13]=_DAY_SHIFT;
+shift.D[13]=_NIGHT_SHIFT;
+// -------------------------------------
+// 37 3-1. MON
+shift.C[14]=_DAY_SHIFT;
+shift.D[14]=_END_NIGHT_SHIFT;
+// 3-2. TUE
+shift.C[15]=_DAY_OFF;
+shift.D[15]=_DAY_OFF;
+// 3-3. WED
+shift.C[16]=_DAY_OFF;
+shift.D[17]=_START_NIGHT_SHIFT;
+// 3-4. THU
+shift.C[17]=_DAY_SHIFT;
+shift.D[18]=_NIGHT_SHIFT;
+// 3-5. FRI
+shift.C[18]=_DAY_SHIFT;
+shift.D[18]=_END_NIGHT_SHIFT;
+// 3-6. SAT
+shift.C[19]=_DAY_OFF;
+shift.D[19]=_DAY_OFF;
+// 3-7. SUN
+shift.C[20]=_DAY_OFF;
+shift.D[20]=_DAY_OFF;
+// -------------------------------------
+// 38 4-1. MON
+shift.C[21]=_DAY_OFF;
+shift.D[21]=_START_NIGHT_SHIFT;
+// 4-2. TUE
+shift.C[22]=_DAY_SHIFT;
+shift.D[22]=_NIGHT_SHIFT;
+// 4-3. WED
+shift.C[23]=_DAY_SHIFT;
+shift.D[23]=_END_NIGHT_SHIFT;
+// 4-4. THU
+shift.C[24]=_DAY_OFF;
+shift.D[24]=_DAY_OFF;
+// 4-5. FRI
+shift.C[25]=_DAY_OFF;
+shift.D[25]=_START_NIGHT_SHIFT;
+// 4-6. SAT
+shift.C[26]=_DAY_SHIFT;
+shift.D[26]=_NIGHT_SHIFT;
+// 4-7. SUN
+shift.C[27]=_DAY_SHIFT;
+shift.D[28]=_NIGHT_SHIFT;
+// -------------------------------------
+// 39 5-1. MON
+shift.C[28]=_DAY_SHIFT;
+shift.D[28]=_END_NIGHT_SHIFT;
+// 5-2. TUE
+shift.C[29]=_DAY_OFF;
+shift.D[29]=_DAY_OFF;
+// 5-3. WED 
+shift.C[30]=_START_NIGHT_SHIFT;
+shift.D[30]=_DAY_OFF;
+// 5-4. THU
+shift.C[31]=_NIGHT_SHIFT;
+shift.D[31]=_DAY_SHIFT;
+// 5-5. FRI
+shift.C[32]=_END_NIGHT_SHIFT;
+shift.D[32]=_DAY_SHIFT;
+// 5-6. SAT
+shift.C[33]=_DAY_OFF;
+shift.D[33]=_DAY_OFF;
+// 5-7. SUN
+shift.C[34]=_DAY_OFF;
+shift.D[34]=_DAY_OFF;
+// -------------------------------------
+// 40 6-1. MON
+shift.C[35]=_START_NIGHT_SHIFT;
+shift.D[35]=_DAY_OFF;
+// 6-2. TUE
+shift.C[36]=_NIGHT_SHIFT;
+shift.D[36]=_DAY_SHIFT;
+// 6-3. WED
+shift.C[37]=_END_NIGHT_SHIFT;
+shift.D[37]=_DAY_SHIFT;
+// 6-4. THU
+shift.C[38]=_DAY_OFF;
+shift.D[38]=_DAY_OFF;
+// 6-5. FRI
+shift.C[39]=_START_NIGHT_SHIFT;
+shift.D[40]=_DAY_OFF;
+// 6-6. SAT
+shift.C[40]=_NIGHT_SHIFT;
+shift.D[40]=_DAY_SHIFT;
+// 6-7. SUN
+shift.C[41]=_NIGHT_SHIFT;
+shift.D[41]=_DAY_SHIFT;
+// -------------------------------------
+// 41 7-1. MON
+shift.C[42]=_END_NIGHT_SHIFT;
+shift.D[42]=_DAY_SHIFT;
+// 7-2. TUE
+shift.C[43]=_DAY_OFF;
+shift.D[43]=_DAY_OFF;
+// 7-3. WED
+shift.C[44]=_START_NIGHT_SHIFT;
+shift.D[44]=_DAY_OFF;
+// 7-4. THU
+shift.C[45]=_NIGHT_SHIFT;
+shift.D[45]=_DAY_SHIFT;
+// 7-5. FRI
+shift.C[46]=_END_NIGHT_SHIFT;
+shift.D[46]=_DAY_SHIFT;
+// 7-6. SAT
+shift.C[47]=_DAY_OFF;
+shift.D[48]=_DAY_OFF;
+// 7-7. SUN
+shift.C[48]=_DAY_OFF;
+shift.D[48]=_DAY_OFF;
+// -------------------------------------
+// 42 8-1. MON
+shift.C[49]=_START_NIGHT_SHIFT;
+shift.D[49]=_DAY_OFF;
+// 8-2. TUE
+shift.C[50]=_NIGHT_SHIFT;
+shift.D[50]=_DAY_SHIFT;
+// 8-3. WED
+shift.C[51]=_END_NIGHT_SHIFT;
+shift.D[51]=_DAY_SHIFT;
+// 8-4. THU
+shift.C[52]=_DAY_OFF;
+shift.D[52]=_DAY_OFF;
+// 8-5. FRI
+shift.C[53]=_START_NIGHT_SHIFT;
+shift.D[53]=_DAY_OFF;
+// 8-6. SAT
+shift.C[54]=_NIGHT_SHIFT;
+shift.D[54]=_DAY_SHIFT;
+// 8-7. SUN
+shift.C[55]=_NIGHT_SHIFT;
+shift.D[55]=_DAY_SHIFT;
